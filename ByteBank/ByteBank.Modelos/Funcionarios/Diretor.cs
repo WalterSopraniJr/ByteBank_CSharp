c706795f21d58cdaa602725070aa6196a5d303d1
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-06-07 05:48:00 -0300

    Iniciando novo repositorio baseado na parte 5 da aula. Criando uma biblioteca com todo o sistema ja desenvolvido.

---
+++ ByteBank/ByteBank.Modelos/Funcionarios/Diretor.cs
@@ -0,0 +1,28 @@
+﻿using Funcionarios_ByteBank.Sistemas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcionarios_ByteBank.Funcionarios
+{
+    public class Diretor : FuncionarioAutenticavel
+    {
+        public Diretor(string cpf) : base(5000, cpf)
+        {
+            Console.WriteLine("Criando Diretor");
+        }
+
+        public override void AumentarSalario()
+        {
+            Salario *= 1.15;
+        }
+
+
+        public override double GetBonificacao()
+        {
+            return Salario * 0.5;
+        }
+    }
+}
---
+++ ByteBank/ByteBank.Modelos/Funcionarios/GerenteDeContas.cs
@@ -0,0 +1,29 @@
+﻿using Funcionarios_ByteBank.Sistemas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcionarios_ByteBank.Funcionarios
+{
+    public class GerenteDeConta : FuncionarioAutenticavel
+    {
+
+        public GerenteDeConta(string cpf) : base(4000, cpf)
+        {
+            Console.WriteLine("Criando Gerente de Conta");
+        }
+
+        public override void AumentarSalario()
+        {
+            Salario *= 1.05;
+        }
+
+        public override double GetBonificacao()
+        {
+            return Salario * 0.25;
+        }
+
+    }
+}
